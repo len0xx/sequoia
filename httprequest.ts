605298430dab5dadf5b9872d6a592a02a56cabd5
Prokhor Minin <prokhor@len0xx.space>
2023-03-13 19:04:15 +0500

    Get rid of unused properties on HTTPContextRequest

--- httprequest.ts
+++ httprequest.ts
@@ -5,19 +5,9 @@ import type { RouteParams } from './router.ts'
 export class HTTPContextRequest<
     Params extends Record<string, string> = RouteParams,
 > {
-    readonly cache: RequestCache
-    readonly credentials: RequestCredentials
-    readonly destination: RequestDestination
     readonly headers: Headers
-    readonly integrity: string
-    readonly isHistoryNavigation: boolean
-    readonly isReloadNavigation: boolean
-    readonly keepalive: boolean
     readonly method: string
-    readonly mode: RequestMode
     readonly redirect: RequestRedirect
-    readonly referrer: string
-    readonly referrerPolicy: ReferrerPolicy
     readonly signal: AbortSignal
     readonly url: URL
     readonly body: ReadableStream<Uint8Array> | null
@@ -36,18 +26,8 @@ export class HTTPContextRequest<
     constructor(request: Request, params: Params = {} as Params) {
         this.body = request.body
         this.bodyUsed = request.bodyUsed
-        this.cache = request.cache
-        this.credentials = request.credentials
-        this.destination = request.destination
         this.headers = request.headers
-        this.integrity = request.integrity
-        this.isHistoryNavigation = request.isHistoryNavigation
-        this.isReloadNavigation = request.isReloadNavigation
-        this.keepalive = request.keepalive
-        this.mode = request.mode
         this.redirect = request.redirect
-        this.referrer = request.referrer
-        this.referrerPolicy = request.referrerPolicy
         this.signal = request.signal
         this.url = new URL(request.url)
         this.method = request.method
