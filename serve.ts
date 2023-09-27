08ba184f2cc99107cb4ea6f8ed5984752b430db6
Prokhor Minin <len0xx@icloud.com>
2023-09-27 18:12:25 +0500

    Allow serving the files by using deno run serve.ts

---
+++ serve.ts
@@ -0,0 +1,29 @@
+import { Application } from './mod.ts'
+import { Router } from './router.ts'
+import { parse, stdPath } from './deps.ts'
+
+const pathArg = Deno.args[0]
+const flags = parse(Deno.args, {
+    string: ['hostname', 'port'],
+})
+const HOSTNAME = flags.hostname ?? '0.0.0.0'
+const PORT = flags.port ? +flags.port : 8080
+
+if (!pathArg) {
+    throw new Deno.errors.NotFound(
+        'Please specify a path to serve. Example: deno run -A serve.ts build/static',
+    )
+}
+
+const PATH = stdPath.normalize(pathArg)
+const app = new Application()
+
+app.useRouter('/', new Router().static('/', PATH))
+
+app.listen(
+    { hostname: HOSTNAME, port: PORT },
+    () => {
+        console.log(`Sequoia server is running at http://${HOSTNAME}:${PORT}`)
+        console.log(`Now serving ${PATH}`)
+    },
+)
