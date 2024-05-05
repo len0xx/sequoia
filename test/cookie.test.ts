273d539df9fa4126e57c43d394f7a0112435c224
Prokhor <len0xx@icloud.com>
2024-05-05 12:44:32 +0500

    Linting

--- test/cookie.test.ts
+++ test/cookie.test.ts
@@ -54,7 +54,7 @@ Deno.test('Cookie class', async (t) => {
         const cookie = new Cookie('foo', 'bar', options)
         assertEquals(
             cookie.toString(),
-            `foo=bar; Expires=${ date.toUTCString() }; HttpOnly; Secure; Domain=localhost.com; Path=/; SameSite=lax; Max-Age=12345`,
+            `foo=bar; Expires=${date.toUTCString()}; HttpOnly; Secure; Domain=localhost.com; Path=/; SameSite=lax; Max-Age=12345`,
         )
     })
 })
