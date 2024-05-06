cd0d0e341b5359d3d206163a4ed0929435d83412
Prokhor <len0xx@icloud.com>
2024-05-06 15:47:25 +0500

    Specify std version

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 // Copyright 2023-2024 the Sequoia authors. All rights reserved. MIT license.
 
-export * as stdPath from 'jsr:@std/path'
-export { normalize as normalizePosix } from 'jsr:@std/path/posix'
-export { normalize as normalizeWindows } from 'jsr:@std/path/windows'
-export * as mediaTypes from 'jsr:@std/media-types'
-export * from 'jsr:@std/assert'
-export { isClientErrorStatus, isErrorStatus, isServerErrorStatus } from 'jsr:@std/http'
+export * as stdPath from 'jsr:@std/path@0.223.0'
+export { normalize as normalizePosix } from 'jsr:@std/path@0.223.0/posix'
+export { normalize as normalizeWindows } from 'jsr:@std/path@0.223.0/windows'
+export * as mediaTypes from 'jsr:@std/media-types@0.223.0'
+export * from 'jsr:@std/assert@0.223.0'
+export { isClientErrorStatus, isErrorStatus, isServerErrorStatus } from 'jsr:@std/http@0.223.0'
 export * from 'npm:path-to-regexp'
-export { parseArgs } from 'jsr:@std/cli'
+export { parseArgs } from 'jsr:@std/cli@0.223.0'
